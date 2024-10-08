7aa944636a34db155c77d794b166b9cb386cf3b4
Shuaibu Husseini abdulwahaab <162578296+savagekvng@users.noreply.github.com>
2024-10-08 13:50:07 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,58 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('fa-xmark');
+  navbar.classList.toggle('active');
+};
+
+let section = document.querySelector('section');
+let navLinks = document.querySelector('header nav a');
+
+window.onscroll = () => {
+  section.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach.apply(links => {
+        links.classList.remove('active');
+        document
+          .querySelector('header nav a{href*=' + id + ']')
+          .classList.add('active');
+      });
+    }
+  });
+
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  menuIcon.classList.remove('fa-xmark');
+  navbar.classList.remove('active');
+};
+
+ScrollReveal({
+  distance: '80px',
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal(
+  '.home-img, .services-container, .portfolio-box, contact form',
+  {
+    origin: 'bottom',
+  }
+);
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+
+const typed = new typed('.multiple-text', {
+  strings: ['Frontend Developer', 'Web designer'],
+  typeSpeed: 70,
+  backspeed: 70,
+  backDelay: 1000,
+  loop: true,
+});
